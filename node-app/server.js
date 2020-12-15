5f9830502c723bced5b5235c47d288b41f534518
Achintha Ikiriwatte <achintha.ikiriwatte@outlook.com>
2020-12-16 00:48:40 +0530

    added lambda node app

---
+++ node-app/server.js
@@ -0,0 +1,27 @@
+var AWS = require('aws-sdk');
+var dynamo = new AWS.DynamoDB.DocumentClient();
+exports.handler = function (event, context, callback) {
+    var operation = event.operation;
+    if (event.tableName) {
+        event.payload.TableName = event.tableName;
+    }
+    switch (operation) {
+        case 'create':
+            dynamo.put(event.payload, callback);
+            break;
+        case 'read':
+            dynamo.get(event.payload, callback);
+            break;
+        case 'update':
+            dynamo.update(event.payload, callback);
+            break;
+        case 'delete':
+            dynamo.delete(event.payload, callback);
+            break;
+        case 'list':
+            dynamo.scan(event.payload, callback);
+            break;
+        default:
+            callback('Unknown operation: ${operation}');
+    }
+};
